b7068eff87240d7fada556e77845b63613a62beb
James Macapagal <j.macapagal@gmail.com>
2022-07-07 10:52:20 -0500

    finished CC, refactoring

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,13 @@
 class Company < ActiveRecord::Base
     has_many :freebies
     has_many :devs, through: :freebies
+
+    def give_freebie(dev,item_name,value)
+        dev_id = Dev.find_by(name: dev).id
+        Freebie.create(item_name: item_name,value: value, company_id: self.id,dev_id: dev_id)
+    end
+
+    def self.oldest_company
+        self.minimum(:founding_year)
+    end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,19 @@
 class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
+
+    def received_one(item_name)
+       received_item_dev_id = Freebie.find_by(item_name: item_name).dev_id
+       if received_item_dev_id == self.id
+        true
+       else
+        false
+       end
+    end
+
+    def give_away(dev,freebie)
+        dev_id = Dev.find_by(name: dev).id
+        freebie_id = Freebie.find_by(item_name: freebie).id
+        Freebie.update(freebie_id,dev_id: dev_id)
+    end
 end
